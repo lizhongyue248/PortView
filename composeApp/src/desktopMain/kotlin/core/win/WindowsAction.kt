3e3935cd8e6841efd35f999a0d155124d2389fd5
lizhongyue <lizhongyue248@163.com>
2024-01-19 21:02:38 +0800

    🧑‍💻 Improve developer experience.

--- composeApp/src/desktopMain/kotlin/core/win/WindowsAction.kt
+++ composeApp/src/desktopMain/kotlin/core/win/WindowsAction.kt
@@ -3,18 +3,16 @@ package core.win
 import core.ActionStrategy
 import org.tinylog.kotlin.Logger
 
-object WindowsAction : ActionStrategy {
-  override fun closeProcess(pid: Int?): Pair<Boolean, String> {
-    if (pid == null) {
-      return Pair(false, "No find process $pid")
-    }
-    val process = ProcessHandle.of(pid.toLong())
-    if (process.isEmpty) {
-      Logger.error("[Error] Can not find process $pid. Maybe you should use root.")
-      return Pair(false, "Can not find process \$$pid.")
-    }
-    val result = process.get().destroyForcibly()
-    Logger.info("Close process result $result.")
-    return Pair(true, "Success")
+val WindowsAction = ActionStrategy { pid: Int? ->
+  if (pid == null) {
+    return@ActionStrategy Pair(false, "No find process $pid")
   }
+  val process = ProcessHandle.of(pid.toLong())
+  if (process.isEmpty) {
+    Logger.error("[Error] Can not find process $pid. Maybe you should use root.")
+    return@ActionStrategy Pair(false, "Can not find process \$$pid.")
+  }
+  val result = process.get().destroyForcibly()
+  Logger.info("Close process result $result.")
+  return@ActionStrategy Pair(true, "Success")
 }
