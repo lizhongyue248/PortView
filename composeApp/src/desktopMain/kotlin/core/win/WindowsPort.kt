0d1d3fc067c050d4e70768d7d2a84dbc8d9bec5e
lizhongyue <lizhongyue248@163.com>
2024-01-27 13:07:04 +0800

    ✨ Add admin privilege for win.

--- composeApp/src/desktopMain/kotlin/core/win/WindowsPort.kt
+++ composeApp/src/desktopMain/kotlin/core/win/WindowsPort.kt
@@ -1,6 +1,10 @@
 package core.win
 
+import com.sun.jna.Native
+import com.sun.jna.platform.win32.*
+import core.Platform
 import core.PortStrategy
+import org.tinylog.kotlin.Logger
 import oshi.software.os.windows.WindowsInternetProtocolStats
 import java.awt.image.BufferedImage
 import java.io.File
@@ -11,6 +15,16 @@ import javax.swing.filechooser.FileSystemView
 object WindowsPort : PortStrategy() {
   private val stats = WindowsInternetProtocolStats()
   private val systemView = FileSystemView.getFileSystemView()
+
+  init {
+    if (!enableDebugPrivilege()) {
+      Platform.isDebug = false
+      Logger.info("Current os doesn't enable debug privilege.")
+    } else {
+      Platform.isDebug = true
+      Logger.info("Current os enable debug privilege.")
+    }
+  }
 
   override fun getInternetProtocolStats() = stats
 
@@ -30,5 +44,41 @@ object WindowsPort : PortStrategy() {
     g2d.dispose()
     return bufferedImage
   }
+
+  private fun enableDebugPrivilege(): Boolean {
+    val hToken = WinNT.HANDLEByReference()
+    var success = Advapi32.INSTANCE.OpenProcessToken(
+      Kernel32.INSTANCE.GetCurrentProcess(),
+      WinNT.TOKEN_QUERY or WinNT.TOKEN_ADJUST_PRIVILEGES, hToken
+    )
+    if (!success) {
+      Logger.debug("OpenProcessToken 1 failed. Error: ${Native.getLastError()}")
+      return false
+    }
+    try {
+      val luid = WinNT.LUID()
+      success = Advapi32.INSTANCE.LookupPrivilegeValue(null, WinNT.SE_DEBUG_NAME, luid)
+      if (!success) {
+        Logger.debug("OpenProcessToken 2 failed. Error: ${Native.getLastError()}")
+        return false
+      }
+      val tkp = WinNT.TOKEN_PRIVILEGES(1)
+      tkp.Privileges[0] = WinNT.LUID_AND_ATTRIBUTES(luid, WinDef.DWORD(WinNT.SE_PRIVILEGE_ENABLED.toLong()))
+      success = Advapi32.INSTANCE.AdjustTokenPrivileges(hToken.value, false, tkp, 0, null, null)
+      val err = Native.getLastError()
+      if (!success) {
+        Logger.debug("OpenProcessToken 3 failed. Error: $err")
+        return false
+      } else if (err == WinError.ERROR_NOT_ALL_ASSIGNED) {
+        Logger.debug("Debug privileges not enabled.")
+        return false
+      }
+    } finally {
+      Kernel32.INSTANCE.CloseHandle(hToken.value)
+    }
+    Logger.debug("Debug privileges enabled.")
+    return true
+  }
+
 }
 
